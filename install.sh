efb5a6efae0b8fadb1c4b41c1514921fca288a5a
lluis <lluis@ngynx.com>
2017-10-30 21:14:05 +0100

    added done comment at end

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ export VIMRC_LOCATION=$INSTALL_DIR/.vimrc
 export VIM_DIR=$INSTALL_DIR/.vim
 
 uninstall(){
-	rm $VIMRC_LOCATION -f 2> /dev/null
-	rm $VIM_DIR -rf 2> /dev/null
+	rm "$VIMRC_LOCATION" -f 2> /dev/null
+	rm "$VIM_DIR" -rf 2> /dev/null
 }
 
 forceinstall(){
@@ -18,11 +18,11 @@ reinstall(){
 }
 
 install(){
-	cp -r $PWD/.vimrc $PWD/.vim $INSTALL_DIR
+	cp -r "$PWD/.vimrc" "$PWD/.vim" $INSTALL_DIR
 }
 
-echo "Trying to install vim config into $INSTALL_DIR"
-if test -f $VIMRC_LOCATION || test -d $VIM_DIR;then
+echo "Trying to install vim config into $INSTALL_DIR ..."
+if test -f "$VIMRC_LOCATION" || test -d "$VIM_DIR";then
 	echo "Warning: vim config files already present ($VIMRC_LOCATION or $VIM_DIR)"
 	read -p "Do you want to replace it? (yes/*): " replace
 	if [[ "$replace" == "yes" ]];then
@@ -34,4 +34,5 @@ if test -f $VIMRC_LOCATION || test -d $VIM_DIR;then
 else
 	install
 fi
+echo "Done"
 
